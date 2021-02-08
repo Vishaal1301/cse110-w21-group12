7022a3c589ca802fe360c54edc13ec1a28e55a9a
Kevin <kevin@Landis-MacBook-Pro.local>
2021-02-07 18:25:52 -0800

    added comment to sum.js

--- sum.js
+++ sum.js
@@ -1,3 +1,9 @@
+/**
+ * Sums 2 numbers together
+ * @param {number} a - The value of the first number.
+ * @param {number} a - The value of the second number.
+ * @returns {number} - The sum of a and b
+ */
 function sum(a, b) {
     return a + b;
 }
