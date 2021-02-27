54ab2069d2f7dec0a26c3995bfe6bbf2c15b143f
tristinxie <tristinxie@gmail.com>
2021-02-27 13:12:52 -0800

    removed taskbar

--- source/app.js
+++ source/app.js
@@ -10,28 +10,6 @@ let mouseOver = false;
 //     updateTimerSettings(clock, 10, 5, 7);
 // }
 
-
-const showTasks = () => {
-    //hide timer
-    let timerDiv = document.getElementById("timerContainer");
-    timerDiv.style.display = "none";
-    //show task list
-    let taskListDiv = document.getElementById("taskListContainer");
-    taskListDiv.style.display = "block";
-}
-
-const showTimer = () => {
-    //hide task list
-    let taskListDiv = document.getElementById("taskListContainer");
-    taskListDiv.style.display = "none";
-    //show timer
-    let timerDiv = document.getElementById("timerContainer");
-    timerDiv.style.display = "flex";
-}
-
-document.getElementById("timerNav").addEventListener("click", showTimer);
-document.getElementById("tasksNav").addEventListener("click", showTasks);
-
 const updateCoffeeCup = () => {
     if(isCountdown) {
         session.innerHTML = "Stop Brewing?"
@@ -53,14 +31,6 @@ cup.onmouseleave = () => {
 }
 
 cup.onclick = () => {
-    if(!isCountdown){
-        showTimer();
-        document.getElementById("toolBar").style.display = "none";
-        document.getElementById("navBar").style.display = "none";
-    } else{
-        document.getElementById("toolBar").style.display = "block";
-        document.getElementById("navBar").style.display = "block"; 
-    }
     startStopTimer(clock, (state) => {
         currentState = state;
         if(!mouseOver){
