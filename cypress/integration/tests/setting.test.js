19ec4ffbcd60de4d2d5dfc743a98ac06295876f6
Kevin Luo <37989506+kevinyluo@users.noreply.github.com>
2021-03-08 21:41:54 -0800

    Update setting.test.js

--- cypress/integration/tests/setting.test.js
+++ cypress/integration/tests/setting.test.js
@@ -1,6 +1,6 @@
 describe('Setting test', () => {
     beforeEach(() => {
-      cy.visit("http://127.0.0.1:5500/source_test/index.html"); // TODO: change URL when deploy
+      cy.visit("http://127.0.0.1:5500/source/index.html"); // TODO: change URL when deploy
     });
     
     describe("open/close setting window test", () => {
