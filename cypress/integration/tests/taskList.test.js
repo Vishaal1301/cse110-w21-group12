17d147dc3d6fd120cc443d917d519e6521890c8b
Kevin Luo <kevinyluo@gmail.com>
2021-03-13 11:14:00 -0800

    First Commit

--- cypress/integration/tests/taskList.test.js
+++ cypress/integration/tests/taskList.test.js
@@ -383,7 +383,7 @@ describe("TaskList test", () => {
                 .get("#tasks")
                 .find(".taskItem")
                 .find(".dropdown")
-                .click({force: true});
+                .click();
             
             cy.get("#new-task")
                 .get("#tasks")
